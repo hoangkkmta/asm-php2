3876367991698a5f95c9160d6c4d0ff46fc3a53f
hoangkk <48815766+hoangkkmta@users.noreply.github.com>
2020-04-24 22:40:14 +0700

    Create showcategory.php

---
+++ app/views/showcategory.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang chủ</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+<body>
+<h1>Hiển thị danh mục</h1>
+
+    <table class="table table-stripped">
+        <thead>
+            <th>ID</th>
+            <th>Cate_name</th>
+            
+        </thead>
+        <tbody>
+            <?php foreach($category as $pro):?>
+                <tr>
+                    <td><?= $pro['id']?></td>
+                    <td><?= $pro['cate_name']?></td>
+                   
+                </tr>
+            <?php endforeach;?>
+        </tbody>
+    </table>
+    
+    
+</body>
+</html>
