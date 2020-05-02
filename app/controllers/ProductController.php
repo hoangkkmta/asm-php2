50005a63ce53b503921341602dc1ae54dffa1f8b
hoangkk <48815766+hoangkkmta@users.noreply.github.com>
2020-05-02 21:56:14 +0700

    Update ProductController.php

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -1,13 +1,34 @@
 <?php
 namespace App\Controllers;
-class ProductController{
+use App\Models\Category;
+use App\Models\Product;
+class ProductController extends BaseController{
    
-
+   
     public function addForm(){
-        // $products = Product::all();
-        // header('location: /');
-        // die;
-        include_once './app/views/addproduct.php';
+        // lấy dữ liệu từ bảng danh mục
+        $cates = Category::all();
+        $this->render('product.add-form', ['cates' => $cates]);
+    }
+
+    public function saveadd(){
+        $requestData = $_POST;
+        $imgFile = $_FILES['image'];
+
+        $model = new Product();
+
+        $model->fill($requestData);
+
+        $filename = "";
+        // nếu có ảnh up lên thì lưu ảnh
+        if($imgFile['size'] > 0){
+            $filename = uniqid() . '-' . $imgFile['name'];
+            move_uploaded_file($imgFile['tmp_name'], './public/uploads/' . $filename);
+            $filename = 'public/uploads/' . $filename;
+        }
+        $model->image = $filename;
+        $model->save();
+        header('location: ./');
     }
 
 
