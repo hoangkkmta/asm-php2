fac8a2484ab6e0c3924dc07bc848f8ca8d9a56e8
hoangkk <48815766+hoangkkmta@users.noreply.github.com>
2020-05-02 21:57:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,8 @@
 session_start();
 $url = isset($_GET['url']) ? $_GET['url'] : "/";
 
-require_once './app/controllers/HomeController.php';
-require_once './app/controllers/ProductController.php';
-require_once './app/controllers/CategoryController.php';
-
-
-require_once './app/models/BaseModel.php';
-require_once './app/models/Product.php';
-require_once './app/models/Category.php';
+require_once './vendor/autoload.php';
+require_once './commons/database-config.php';
 
 use App\Controllers\HomeController;
 use App\Controllers\ProductController;
@@ -17,27 +11,34 @@ use App\Controllers\CategoryController;
 switch ($url) {
     case '/':
         $ctr = new HomeController();
-        echo $ctr->showDetail();
+         $ctr->index();
         break;
     
     case 'add-product':
         $ctr = new ProductController();
-        echo $ctr->addForm();
+         $ctr->addForm();
+        break; 
+
+     case 'save-add-product':
+        $ctr = new ProductController();
+         $ctr->saveadd();
         break;
-    
+
     case 'categories':
         $ctr = new CategoryController();
-        echo $ctr->showCategory();
+         $ctr->showCategory();
         break;
-    case 'categories/add':
-         $ctr = new CategoryController();
-         echo $ctr->addForm();
-         break;
+    
 
-    // case 'add-product':
-    //     $ctr = new ProductController();
-    //     echo $ctr->addForm();
-    //     break;
+    case 'add-categories':
+        $ctr = new CategoryController();
+        echo $ctr->addForm();
+        break;
+
+     case 'save-add-cate':
+        $ctr = new CategoryController();
+        $ctr->saveadd();
+        break;
     default:
         echo "Đường dẫn không tồn tại";
         break;
