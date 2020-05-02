6492a09cf420061ebe5617353a707f1369286e0a
hoangkk <48815766+hoangkkmta@users.noreply.github.com>
2020-05-02 21:55:58 +0700

    Update HomeController.php

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,15 +1,14 @@
 <?php
 namespace App\Controllers;
 use App\Models\Product;
-class HomeController{
-
-    public function showDetail(){
+class HomeController extends BaseController{
+    public function index(){
         $products = Product::all();
-        // header('location: /');
-        // die;
-        include_once './app/views/homepage.php';
+
+        $this->render('homepage.index', ['listItem' => $products]); //ten bien ton tai o file view
     }
 
+   
     
 
 }
