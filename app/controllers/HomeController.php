e32efc67689492c22aea245f9c35ab256ec85b2b
hoangkk <48815766+hoangkkmta@users.noreply.github.com>
2020-05-03 10:18:20 +0700

    Update HomeController.php

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -4,8 +4,12 @@ use App\Models\Product;
 class HomeController extends BaseController{
     public function index(){
         $products = Product::all();
+        $msg = isset($_GET['msg']) ? $_GET['msg'] : null;
 
-        $this->render('homepage.index', ['listItem' => $products]); //ten bien ton tai o file view
+        $this->render('homepage.index', ['listItem' => $products,
+                                         'errMsg' => $msg
+        
+                                         ]); //ten bien ton tai o file view
     }
 
    
