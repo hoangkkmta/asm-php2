618cd20f83a305ee9a113cb9f32fb0712baa3341
hoangkk <48815766+hoangkkmta@users.noreply.github.com>
2020-05-03 11:05:33 +0700

    Create a0c2716ce129855ed381ad857c3eecfe5d28946f.php

---
+++ storage/a0c2716ce129855ed381ad857c3eecfe5d28946f.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Edit</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+<body>
+
+    <div class="container">
+        <nav class="nav">
+            <a class="nav-link" href="./">Quản lý sản phẩm</a>
+            <a class="nav-link" href="./add-product">Thêm sản phẩm</a>
+            <a class="nav-link" href="./categories">Quản lý danh mục</a>
+            <a class="nav-link" href="./add-categories">Thêm danh mục</a>
+        </nav>
+        
+        
+        <h3>Tạo mới danh mục</h3>
+       
+        <form action="./save-edit-categories?id=<?php echo e($model->id); ?>" method="POST" enctype="multipart/form-data">
+            <div class="row">
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="">Tên danh mục</label>
+                        <input type="text" name="cate_name" class="form-control" value="<?php echo e($model->cate_name); ?>">
+                    </div>
+                   
+                <div class=" col-12 text-center">
+                    <button type="submit" class="btn btn-info">Lưu</button>
+                    <a href="./" class="btn btn-danger">Hủy</a>
+                </div>
+            </div>
+        </form>
+    </div>
+    
+    
+</body>
+</html><?php /**PATH C:\xampp\htdocs\php\asm-php2\app\views/categories/edit-form.blade.php ENDPATH**/ ?>
